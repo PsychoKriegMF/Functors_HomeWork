8c45455ed8bfffe3ea26a0a4a04759f22e0f2a1e
MishGun94 <kolczov9494@gmail.com>
2024-03-17 21:14:31 +0500

    Добавьте файлы проекта.

---
+++ Functors_HomeWork.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+
+int main() 
+{
+	setlocale(LC_ALL, "ru");
+
+	double num1, num2;
+	char sym;	
+	double result;
+	std::cout << "Введите два числа -> ";
+	std::cin >> num1 >> num2;
+	std::cout << std::endl;
+	std::cout << "Выбирете одну из доступных операций: +,-,*,/ -> ";
+	std::cin >> sym;
+	
+	switch (sym)
+	{
+		case '+':
+			result = [](double num1, double num2)->double { return num1 + num2; }(num1, num2);
+			std::cout << "Результат операции сложения:" << result << std::endl;
+			break;
+		case '-': 
+			result = [](double num1, double num2)->double { return num1 - num2; }(num1, num2);
+			std::cout << "Результат операции вычитания:" << result << std::endl;
+			break;
+		case '*': 
+			result = [](double num1, double num2)->double { return num1 * num2; }(num1, num2);
+			std::cout << "Результат операции умножения:" << result << std::endl;
+			break;
+		case '/': 
+			result = [](double num1, double num2)->double { return num1 / num2; }(num1, num2);
+			std::cout << "Результат операции деления:" << result << std::endl;
+			break;
+		default: std::cout << "Выбранная операция недоступна!\n";
+	}
+	
+	return 0;
+}
